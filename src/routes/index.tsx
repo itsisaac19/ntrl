c7561883f693615ac741c83c485aff20cd2b3c0f
itsisaac19 <64609807+itsisaac19@users.noreply.github.com>
2023-09-10 18:13:46 -0500

    Revamp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,102 +1,113 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
-
 export default component$(() => {
+  const videoRef = useSignal<HTMLVideoElement>();
+  const video = videoRef.value;
+
+  const canvasRef = useSignal<HTMLCanvasElement>();
+  const canvas = canvasRef.value;
+
+  const stripRef = useSignal<HTMLElement>();
+  const strip = stripRef.value;
+
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: 'user'
+    }
+  }
+
+  const getVideo = $(() => {
+    if (!video) return
+    
+    console.log('hello?')
+    
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(localMediaStream => {
+        console.log(localMediaStream);
+      
+  //  DEPRECIATION : 
+  //       The following has been depreceated by major browsers as of Chrome and Firefox.
+  //       video.src = window.URL.createObjectURL(localMediaStream);
+  //       Please refer to these:
+  //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+  //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+        console.dir(video);
+        if ('srcObject' in video) {
+          video.srcObject = localMediaStream;
+        } else {
+          //video.src = URL.createObjectURL(localMediaStream);
+        }
+        // video.src = window.URL.createObjectURL(localMediaStream);
+        video.play();
+      })
+      .catch(err => {
+        console.error(`OH NO!!!!`, err);
+      });
+  })
+  
+  const paintToCanvas = $(() => {
+    if (!video || !canvas) return;
+
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+  
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, width, height);
+      // take the pixels out
+      // let pixels = ctx.getImageData(0, 0, width, height);
+      // mess with them
+      // pixels = redEffect(pixels);
+  
+      // pixels = rgbSplit(pixels);
+      // ctx.globalAlpha = 0.8;
+  
+      // pixels = greenScreen(pixels);
+      // put them back
+      // ctx.putImageData(pixels, 0, 0);
+    }, 16);
+  })
+  
+  const takePhoto = $(() => {
+    if (!canvas || !strip) return;
+  
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+  })
+
+  useVisibleTask$(() => {
+    if (video) {
+      // Fix for iOS Safari from https://leemartin.dev/hello-webrtc-on-safari-11-e8bcb5335295
+      video.setAttribute('autoplay', '');
+      video.setAttribute('muted', '');
+      video.setAttribute('playsinline', '')
+
+      getVideo();
+
+      video.addEventListener('canplay', paintToCanvas);
+    }
+  })
+
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
+      <div class="controls">
+        <button onClick$={takePhoto}>Take Photo</button>
       </div>
 
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
+      <canvas ref={canvasRef} class="photo"></canvas>
+      <video ref={videoRef} class="player"></video>
+      <div ref={stripRef} class="strip"></div>
     </>
   );
 });
