58d595d18f6615ec82899e5ddef264131637855e
itsisaac19 <64609807+itsisaac19@users.noreply.github.com>
2023-10-21 12:50:20 -0500

    nodeExternals

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -2,16 +2,20 @@ import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vi
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 
+import nodeExternals from 'rollup-plugin-node-externals';
+
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
-        external: ['@google-cloud']
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
-    plugins: [vercelEdgeAdapter()],
+    plugins: [
+      vercelEdgeAdapter(),
+      nodeExternals(),
+    ],
   };
 });
