e671457113604b4465b4dcd11924c1ae93507b09
itsisaac19 <64609807+itsisaac19@users.noreply.github.com>
2023-10-21 12:58:37 -0500

    Update vite.config.ts

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -2,13 +2,45 @@ import { vercelEdgeAdapter } from "@builder.io/qwik-city/adapters/vercel-edge/vi
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
 
+const builtInModules = [
+  'assert',
+  'buffer',
+  'child_process',
+  'cluster',
+  'crypto',
+  'dgram',
+  'dns',
+  'domain',
+  'events',
+  'fs',
+  'http',
+  'https',
+  'net',
+  'os',
+  'path',
+  'punycode',
+  'querystring',
+  'readline',
+  'repl',
+  'stream',
+  'string_decoder',
+  'timers',
+  'tls',
+  'tty',
+  'url',
+  'util',
+  'v8',
+  'vm',
+  'zlib',
+];
+
 export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
-        external: ['fs', 'path', 'util', 'stream', 'url', 'crypto']
+        external: builtInModules,
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
