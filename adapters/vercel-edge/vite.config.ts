27c6014181bc5a60931f09fe1c27b29e236ca5a4
itsisaac19 <64609807+itsisaac19@users.noreply.github.com>
2023-10-21 12:25:38 -0500

    LMFAO

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -8,7 +8,7 @@ export default extendConfig(baseConfig, () => {
       ssr: true,
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
-        external: ['fs', 'path']
+        external: ['fs', 'path', 'util']
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
